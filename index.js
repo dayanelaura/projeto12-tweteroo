959168c2e52b49c2c62746b5f16ee16754995a6b
Dayane <dayanelaura3@gmail.com>
2022-11-06 11:03:50 -0300

    Adicionadas as validações, os status code e um  ajuste na dinâmica do GET

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ app.use(express.json());
 
 let usuarios = [];
 let tweets = [];
-let dezTweets = [];
+let ultimosTweets = [];
 
 /* {
     "username": "bobesponja",
@@ -20,37 +20,47 @@ let dezTweets = [];
 
 app.post('/sign-up', (request, response) => {
     const usuario = request.body;
+    const { username, avatar } = request.body;
+
+    if(!username || !avatar)
+    return response.status(400).send('Todos os campos são obrigatórios');
 
     usuarios.push(usuario);
-    response.send("OK");
     console.log(usuarios);
+    return response.status(201).send("OK");
 });
 
 app.post('/tweets', (request, response) => {
-    const tweet = request.body;
+    const tweetObject = request.body;
+    const { username, tweet } = request.body;
 
-    tweets.push(tweet);
-    response.send("OK");
+    if(!username || !tweet)
+    return response.status(400).send('Todos os campos são obrigatórios');
+
+    tweets.push(tweetObject);
     console.log(tweets);
+    return response.status(201).send("OK");
 });
 
 app.get("/tweets", (request, response) => {
-
     const ultimoTweet = tweets.length-1;
-    const primeiroTweet = (tweets.length-10);
+    
+    if(tweets.length<10){
+        for (let i = ultimoTweet; i>=0; i--){
+            const tweet = tweets[i];
+            ultimosTweets.push(tweet);
+            //lembrando que não vem com o avatar e tem que adicionar de alguma forma!
+        };
+    }else{
+        const primeiroTweet = (tweets.length-10);
+        for (let i = ultimoTweet; i>=primeiroTweet; i--){
+            const tweet = tweets[i];
+            ultimosTweets.push(tweet);
+            //lembrando que não vem com o avatar e tem que adicionar de alguma forma!
+        };
+    }
 
-    for (let i = ultimoTweet; i>=primeiroTweet; i--){
-        const tweet = [
-            {
-                username: "bobesponja",
-                avatar: "https://super.abril.com.br/wp-content/uploads/2020/09/04-09_gato_SITE.jpg?quality=70&strip=info",
-                tweet: "eu amo o hub"
-            }
-        ];
-        dezTweets.push(tweet);
-    };
-
-    response.send(dezTweets);
+    response.send(ultimosTweets);
 });
 
 app.listen(5000, () => {
